c3c32c384c530f4d2003fe6ed56ee034d043e9a7
Selene <selenedanyluk@gmail.com>
2025-04-04 16:31:20 -0300

    add toasts notifications

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -6,6 +6,8 @@ import { UserContext } from '../../context/UserContext';
 import Cookies from 'js-cookie';
 import { jwtDecode } from "jwt-decode";
 import { Navigate, useNavigate } from 'react-router';
+import { errorToast, successToast } from "../../ui/toast/NotificationToast";
+import { ToastContainer } from 'react-toastify';
 
 const Login = () => {
     const [email, setEmail] = useState("");
@@ -20,6 +22,7 @@ const Login = () => {
     const { handleUser, setIsLoggedIn } = useContext(UserContext);
     const navigate = useNavigate();
 
+
     const handleEmailChange = (event) => {
         setEmail(event.target.value);
         setErrors({ ...errors, email: false });
@@ -32,38 +35,41 @@ const Login = () => {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-    
+
         if (!emailRef.current.value.length) {
             setErrors({ ...errors, email: true });
             alert("El email es requerido");
             emailRef.current.focus();
+            errorToast("El email es requerido");
             return;
         }
         if (!password || password.length < 7) {
             setErrors({ ...errors, password: true });
             passwordRef.current.focus();
+            errorToast("La contraseña debe tener al menos 7 caracteres");
             return;
         }
-    
+
         const userData = { email, password };
-    
+
         try {
             const responseData = await PostData(userData);
-    
+
             if (responseData?.token) {
                 const token = responseData.token;
                 Cookies.set("token", token, { expires: 7 });
-    
+
                 const decoded = jwtDecode(token);
-    
+
                 handleUser({
                     id: decoded.sub,
                     role: decoded.role,
                     name: decoded.given_name,
                     email: decoded.email || decoded.given_name
                 });
-    
+
                 setIsLoggedIn(true);
+                successToast("Bienvenido/a " + decoded.given_name);
                 setEmail("");
                 setPassword("");
                 navigate("/");
@@ -73,51 +79,51 @@ const Login = () => {
         }
     };
 
-  return (
-    <div className='justify-content-center w-50 justify-content-center mx-auto mt-5'>
-        <Card className="mt-5 mx-3 p-3 px-5 shadow">
-            <Card.Body>
-                <Row className="mb-2">
-                    <h5>¡Bienvenidos/as!</h5>
-                </Row>
-                <Form onSubmit={handleSubmit}>
-                    <FormGroup className="mb-4">
-                        <Form.Control
-                            type="email"
-                            ref={emailRef}
-                            required
-                            placeholder="Ingresar email"
-                            onChange={handleEmailChange}
-                            value={email} 
-                            className={errors.email  && " border border-danger"}
+    return (
+        <div className='justify-content-center w-50 justify-content-center mx-auto mt-5'>
+            <Card className="mt-5 mx-3 p-3 px-5 shadow">
+                <Card.Body>
+                    <Row className="mb-2">
+                        <h5>¡Bienvenidos/as!</h5>
+                    </Row>
+                    <Form onSubmit={handleSubmit}>
+                        <FormGroup className="mb-4">
+                            <Form.Control
+                                type="email"
+                                ref={emailRef}
+                                required
+                                placeholder="Ingresar email"
+                                onChange={handleEmailChange}
+                                value={email}
+                                className={errors.email && " border border-danger"}
                             />
                             {errors.email && <p className="text-danger">El email es requerido</p>}
-                    </FormGroup>
-                    <FormGroup className="mb-4">
-                        <Form.Control
-                            type="password"
-                            ref={passwordRef}
-                            required
-                            placeholder="Ingresar contraseña"
-                            onChange={handlePasswordChange}
-                            value={password}
-                            className={errors.password  && " border border-danger"}
-                        />
-                        <div className='container'>{errors.password && <p className="text-danger">La contraseña debe tener al menos 7 caracteres</p>}</div>
-                    </FormGroup>
-                    <Row>
-                        <Col />
-                        <Col md={12} className="d-flex justify-content-end ">
-                            <Button variant="secondary" type="submit">
-                                Iniciar sesión
-                            </Button>
-                        </Col>
-                    </Row>
-                </Form>
-            </Card.Body>
-        </Card>
-    </div>
-  );
+                        </FormGroup>
+                        <FormGroup className="mb-4">
+                            <Form.Control
+                                type="password"
+                                ref={passwordRef}
+                                required
+                                placeholder="Ingresar contraseña"
+                                onChange={handlePasswordChange}
+                                value={password}
+                                className={errors.password && " border border-danger"}
+                            />
+                            <div className='container'>{errors.password && <p className="text-danger">La contraseña debe tener al menos 7 caracteres</p>}</div>
+                        </FormGroup>
+                        <Row>
+                            <Col />
+                            <Col md={12} className="d-flex justify-content-end ">
+                                <Button variant="secondary" type="submit">
+                                    Iniciar sesión
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Form>
+                </Card.Body>
+            </Card>
+        </div>
+    );
 };
 
 export default Login
